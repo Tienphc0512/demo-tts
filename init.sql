04aaf64428d67bc8abea5a6593b26f358809bbe9
GAME <ngoctttt2020@gmail.com>
2025-09-30 01:50:14 -0700

    Add table of DB

--- init.sql
+++ init.sql
@@ -1,2 +1,6 @@
 CREATE DATABASE user_service_db;
 CREATE DATABASE plant_service_db;
+CREATE DATABASE simulation_service_db;
+CREATE DATABASE websocket_service_db;
+CREATE DATABASE bug_service_db;
+CREATE DATABASE main_service_db;
